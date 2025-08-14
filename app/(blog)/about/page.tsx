d43fe2140c2c8f4ce7540cfc997644119549d789
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-08-14 12:14:21 +0530

    add basic seo titles to the pages

--- app/(blog)/about/page.tsx
+++ app/(blog)/about/page.tsx
@@ -1,6 +1,14 @@
 import AboutUs2 from '@/components/mvpblocks/about-us-2'
 import { ThemeToggler } from '@/components/ThemeToggler'
+import { Metadata } from 'next';
 import React from 'react'
+
+
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "...",
+};
 
 export default function aboutPage() {
   return (
